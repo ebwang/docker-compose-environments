5bd7c3bc7efe60e12daa6490648a22a736bb190f
Eduardo Wang <eduardo.b.wang@gmail.com>
2023-04-08 17:36:35 -0300

    Adding environment elasticsearch for lab

---
+++ elasticsearch/client-py/client.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from elasticsearch import Elasticsearch
+from faker import Faker
+
+es = Elasticsearch('https://localhost:9200')
+fake = Faker()
+
+for i in range(100):
+    doc = {
+        'author': fake.name(),
+        'text': fake.text(),
+        'timestamp': datetime.now(),
+    }
+    resp = es.index(index="test-index", id=i, document=doc)
+    print(resp['result'])
